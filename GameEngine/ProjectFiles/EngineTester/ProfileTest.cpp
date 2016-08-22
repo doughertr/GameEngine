0171499e7c4d50983ae9e90517b00771296ca9c2
RyansHomePC <RyansHomePC@DESKTOP-J7VLCHV>
2016-08-22 11:02:24 -0600

    bug fixes

--- GameEngine/ProjectFiles/EngineTester/ProfileTest.cpp
+++ GameEngine/ProjectFiles/EngineTester/ProfileTest.cpp
@@ -20,17 +20,28 @@ string getNextToken(ifstream& file)
 
 TEST(Profiler, TestEntryAddition)
 {
+	char* categories[] = 
+	{
+		"Category 1",
+		"Category 2",
+		"Category 3"
+	};
+
 	Profiler profiler;
 	const char* profilerFileName = "profiles.csv";
 	profiler.init(profilerFileName);
 
-	const unsigned int NUM_ENTRIES = 15;
+	const unsigned int NUM_CATEGORIES = sizeof(categories) / sizeof(*categories);
+	const unsigned int NUM_FRAMES = 5;
+	float sampleNum = 0;
 
-	for (float i = 0; i < NUM_ENTRIES; i++) {
+	for (float frame = 0; frame < NUM_FRAMES; frame++) {
 		profiler.newFrame();
-		profiler.addEntry("Category 1", i);
-		profiler.addEntry("Category 2", i);
-		profiler.addEntry("Category 3", i);
+		for (unsigned int cat = 0; cat < NUM_CATEGORIES; cat++)
+		{
+			profiler.addEntry(categories[cat], sampleNum++);
+		}
+
 	}
 	profiler.shutdown();
 	ifstream input(profilerFileName);
@@ -38,7 +49,7 @@ TEST(Profiler, TestEntryAddition)
 	EXPECT_EQ(getNextToken(input), "Category1");
 	EXPECT_EQ(getNextToken(input), "Category2");
 	EXPECT_EQ(getNextToken(input), "Category3");
-	for (unsigned int i = 0; i < NUM_ENTRIES; i++)
+	for (unsigned int i = 0; i < (NUM_FRAMES * NUM_CATEGORIES); i++)
 	{
 		string buffer = getNextToken(input);
 		EXPECT_EQ(atoi(buffer.c_str()), i);
